39f5b4db3fb6bfadd2de7a66e7b275580e6c0dc8
Harshit Passi <hrshtpassi@gmail.com>
2019-03-30 14:49:47 +0530

    [Assignment 5] Implemented solution to problem

--- services-assignment/src/app/active-users/active-users.component.ts
+++ services-assignment/src/app/active-users/active-users.component.ts
@@ -1,15 +1,18 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-active-users',
   templateUrl: './active-users.component.html',
   styleUrls: ['./active-users.component.css']
 })
-export class ActiveUsersComponent {
-  @Input() users: string[];
-  @Output() userSetToInactive = new EventEmitter<number>();
+export class ActiveUsersComponent implements OnInit {
+  users: string[];
 
-  onSetToInactive(id: number) {
-    this.userSetToInactive.emit(id);
+  constructor(private userService: UserService) {}
+
+  ngOnInit() {
+    this.users = this.userService.activeUsers;
   }
+
 }
